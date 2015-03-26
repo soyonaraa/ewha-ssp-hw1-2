a5f9426cd6e91455dc5afcab7ebb565e7b4d9212
narangerelt <soyonaraa@gmail.com>
2015-03-26 17:12:09 +0900

    change to lab 2-2
    changed the cpp file to lab assignment 2-1

--- lass2_1.cpp
+++ lass2_1.cpp
@@ -2,48 +2,45 @@
 #include "lass2_1.h"
 
 
-#define FRONT_LED_PIN 10
+define DUTY_CYCLE 20
 #define REAR_LED_PIN 9
 
-   int val=0;
-   int val1=255;
-   int inc=0;
+int a=0;
 
-//The setup function is called once at startup of the sketch
-void setup() {
-	pinMode(FRONT_LED_PIN, OUTPUT);
-	pinMode(REAR_LED_PIN, OUTPUT);
+void PWM_Write(int pin, int on_time_perc){
+
+	int on_time=DUTY_CYCLE*on_time_perc*0.01;
+	int off_time=DUTY_CYCLE-on_time;
+	digitalWrite(pin,HIGH);
+	delay(on_time);
+	digitalWrite(pin,LOW);
+	delay(off_time);
+}
+
+void myAnalogWrite(int pin, int percent, int time){
+	int numLoops=time/DUTY_CYCLE;
+	int i;
+	for(i=0; i<=numLoops; i++){
+		PWM_Write(pin,percent);
+	}
+}
+void setup()
+{
+	pinMode(REAR_LED_PIN,OUTPUT);
 }
 
 // The loop function is called in an endless loop
-void loop() {
+void loop()
+{
 
-
-        if(inc==0){
-
-        	analogWrite(FRONT_LED_PIN, val);
-        	val++;
-        	analogWrite(REAR_LED_PIN, val1);
-        	val1--;
-        	delay(5000/255);
-        }
-		if(inc==0&&val==255 && val1==0){
-			inc=1;
+	if(a==0){
+		myAnalogWrite(REAR_LED_PIN,100,2000);
+		myAnalogWrite(REAR_LED_PIN,75,2000);
+		myAnalogWrite(REAR_LED_PIN,50,2000);
+		myAnalogWrite(REAR_LED_PIN,25,2000);
 		}
-		if(inc==1){
-			analogWrite(FRONT_LED_PIN, val);
-			val--;
-
-			analogWrite(REAR_LED_PIN, val1);
-			val1++;
-
-			delay(5000/255);
-		}
-		if(inc==1&&val==0 && val1==255){
-			inc=0;
-		}
-
-
-
-
+	a=1;
+	if(a==1){
+		myAnalogWrite(REAR_LED_PIN,0,2000);
+	}
 }
